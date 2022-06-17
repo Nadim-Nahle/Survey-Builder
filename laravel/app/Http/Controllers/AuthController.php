a8a51b8b0ce35a2e1338e463111a202ceff911bb
Nadim <nahlenadim@gmail.com>
2022-06-17 10:17:53 +0300

    Backend Add- Login API

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -35,6 +35,35 @@ class AuthController extends Controller
         ],201);
     }
 
-   
+    //LOGIN CONTROLLER
+    public function login(Request $request){
+
+        $validator = Validator::make($request -> all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if($validator -> fails()){
+            return response()->json($validator->errors(),422);
+        }
+        if(!$token=auth()->attempt($validator->validated())){
+            return response()->json(['error'=>'Unauthorized'],401);
+        }
+        return $this->createNewToken($token);
+    }
+
+    //CREATE NEW JWT TOKEN
+    public function CreateNewToken($token){
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in'=>auth()->factory()->getTTl()*60,
+            'user'=>auth()->user(),
+            
+        ]);
+    }
+
+    
+  
     
 }
