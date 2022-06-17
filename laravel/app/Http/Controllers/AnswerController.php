f32c61f814fed0238c5f5cf6138867a99f6d30d3
Nadim <nahlenadim@gmail.com>
2022-06-17 14:18:13 +0300

    Backend Add- Get answer Controller

--- laravel/app/Http/Controllers/AnswerController.php
+++ laravel/app/Http/Controllers/AnswerController.php
@@ -8,79 +8,18 @@ use App\Http\Requests\UpdateAnswerRequest;
 
 class AnswerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+    
+    public function getAns($id = null){
+        if($id){
+            $answer = answer::find($id);
+        }
+        else{
+            $answer = answer::all();
+        }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreAnswerRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreAnswerRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Answer $answer)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Answer $answer)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateAnswerRequest  $request
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateAnswerRequest $request, Answer $answer)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Answer $answer)
-    {
-        //
+        return response()->json([
+            "status" => "success",
+            "answers" => $answer
+        ],200);
     }
 }
